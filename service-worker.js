f9af1dfbf14568c4b0362eb9a42b10bf6a30525a
Alois Gether <al.gether@gmail.com>
2026-02-09 10:44:08 +0100

    Cache-Version auf 1.0.6 gesetzt

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = "einkaufsliste-cache-v8";
+const CACHE_VERSION = "v1.0.6";
+const CACHE_NAME = "einkaufsliste-" + CACHE_VERSION;
 
 const FILES_TO_CACHE = [
   "./",
@@ -10,14 +11,40 @@ const FILES_TO_CACHE = [
   "./icon-512.png"
 ];
 
+/* INSTALL */
 self.addEventListener("install", event => {
+  self.skipWaiting();
+
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
   );
 });
 
+/* ACTIVATE */
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+/* FETCH */
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request)
+      .then(response => {
+        if (response) return response;
+        return fetch(event.request).catch(() =>
+          caches.match("./index.html")
+        );
+      })
   );
 });
